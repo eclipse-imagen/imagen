754cd77274054cb801a367e87ddd4ad2d4f80ada
David Blasby <david.blasby@geocat.net>
2025-05-27 16:46:50 -0700

    move problematic Rotate, Shear, and SubtractConst to legacy

--- legacy/core/src/main/java/org/eclipse/imagen/operator/SubtractConstDescriptor.java
+++ legacy/core/src/main/java/org/eclipse/imagen/operator/SubtractConstDescriptor.java
@@ -17,17 +17,14 @@
 
 package org.eclipse.imagen.operator;
 
-import java.awt.RenderingHints;
+import org.eclipse.imagen.*;
+import org.eclipse.imagen.registry.RenderableRegistryMode;
+import org.eclipse.imagen.registry.RenderedRegistryMode;
+
+import java.awt.*;
 import java.awt.image.RenderedImage;
 import java.awt.image.renderable.ParameterBlock;
 import java.awt.image.renderable.RenderableImage;
-import org.eclipse.imagen.JAI;
-import org.eclipse.imagen.OperationDescriptorImpl;
-import org.eclipse.imagen.ParameterBlockJAI;
-import org.eclipse.imagen.RenderableOp;
-import org.eclipse.imagen.RenderedOp;
-import org.eclipse.imagen.registry.RenderableRegistryMode;
-import org.eclipse.imagen.registry.RenderedRegistryMode;
 
 /**
  * An <code>OperationDescriptor</code> describing the "SubtractConst" operation.
