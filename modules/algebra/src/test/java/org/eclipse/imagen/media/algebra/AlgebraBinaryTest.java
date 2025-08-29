924b3b43b3eaf4179d1cdf4a89e697e9fa9795d7
Andrea Aime <andrea.aime@gmail.com>
2025-08-29 19:35:09 +0200

    Imageread should use the provided image stream rather than trying to create a new one

--- modules/algebra/src/test/java/org/eclipse/imagen/media/algebra/AlgebraBinaryTest.java
+++ modules/algebra/src/test/java/org/eclipse/imagen/media/algebra/AlgebraBinaryTest.java
@@ -1,10 +1,11 @@
-package org.eclipse.imagen.media.algebra; /* Copyright (c) 2025 Andrea Aime and others
-                                           *
-                                           * All rights reserved. This program and the accompanying materials
-                                           * are made available under the terms of the Apache License, Version 2.0
-                                           * which accompanies this distribution and is available at
-                                           * http://www.opensource.org/licenses/apache2.0.php.
-                                           */
+/* Copyright (c) 2025 Andrea Aime and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, Version 2.0
+ * which accompanies this distribution and is available at
+ * http://www.opensource.org/licenses/apache2.0.php.
+ */
+package org.eclipse.imagen.media.algebra;
 
 import static org.eclipse.imagen.media.algebra.AlgebraDescriptor.Operator.AND;
 import static org.eclipse.imagen.media.algebra.AlgebraDescriptor.Operator.MAX;
