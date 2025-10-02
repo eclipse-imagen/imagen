1f120dbf2e70ee260529a5d829bb476a17d5db71
Andrea Aime <andrea.aime@gmail.com>
2025-10-02 18:44:17 +0200

    Add deprecated subclasses of color/sample models renamed after JAI, to support long term serialized sample images

---
+++ modules/colorconvert/src/main/java/org/eclipse/imagen/media/colorconvert/ColorSpaceJAIExtWrapper.java
@@ -0,0 +1,23 @@
+/* Copyright (c) 2025 Andrea Aime and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, Version 2.0
+ * which accompanies this distribution and is available at
+ * http://www.opensource.org/licenses/apache2.0.php.
+ */
+package org.eclipse.imagen.media.colorconvert;
+
+import org.eclipse.imagen.ColorSpaceImageN;
+
+/**
+ * This class is a subclass of ColorSpaceImageNExtWrapper to work around serialization issues. It is deprecated and
+ * you're not supposed to use it anymore. It is only kept around for backward compatibility. Won't be removed anytime
+ * soon, in order to support long term serialization of ImageLayout descriptions.
+ */
+@Deprecated
+public class ColorSpaceJAIExtWrapper extends ColorSpaceImageNExtWrapper {
+
+    public ColorSpaceJAIExtWrapper(ColorSpaceImageN cs) {
+        super(cs);
+    }
+}
